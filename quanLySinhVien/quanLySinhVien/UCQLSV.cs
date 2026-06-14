ed3dfec6a98410fd321db3b6586af53a05c52c4b
Nguyen The Anh <ntheanh1217@gmail.com>
2026-06-15 00:13:25 +0700

    feat<sinhvien>: Update

--- quanLySinhVien/quanLySinhVien/UCQLSV.cs
+++ quanLySinhVien/quanLySinhVien/UCQLSV.cs
@@ -74,6 +74,82 @@ namespace login
         {
             txtMaSV.Clear();
             txtHoTen.Clear();
+using QuanLySinhVien;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login
+{
+    public partial class UCQLSV : UserControl
+    {
+        private int selectedId = -1;
+        public UCQLSV()
+        {
+            InitializeComponent();
+        }
+
+        private void UCQLSV_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            LoadComboBoxGioiTinh();
+            LoadComboBoxLop();
+        }
+
+        public void LoadData()
+        {
+            DatabaseDataContext db = new DatabaseDataContext();
+            List<tbl_sinhvien> dSSV = db.tbl_sinhviens.ToList();
+            dataGridView1.DataSource = dSSV;
+        }
+
+        public void LoadComboBoxGioiTinh()
+        {
+            cboGioiTinh.Items.Clear();
+            cboGioiTinh.Items.Add("Nam");
+            cboGioiTinh.Items.Add("Nữ");
+            cboGioiTinh.SelectedIndex = -1;
+
+        }
+        public void LoadComboBoxLop()
+        {
+            DatabaseDataContext db = new DatabaseDataContext();
+            List<tbl_lophoc> dSLop = db.tbl_lophocs.ToList();
+            cboLop.DataSource = dSLop;
+            cboLop.DisplayMember = "tenlop";
+            cboLop.ValueMember = "malop";
+            cboLop.SelectedIndex = -1;
+        }
+
+        private void btlThem_Click(object sender, EventArgs e)
+        {
+            DatabaseDataContext db = new DatabaseDataContext();
+            tbl_sinhvien sv = new tbl_sinhvien();
+            sv.masv = txtMaSV.Text;
+            sv.hoten = txtHoTen.Text;
+            sv.gioitinh = cboGioiTinh.SelectedItem.ToString();
+            sv.ngaysinh = dtpNgaySinh.Value;
+            sv.malop = cboLop.SelectedValue.ToString();
+            db.tbl_sinhviens.InsertOnSubmit(sv);
+            db.SubmitChanges();
+            LoadData();
+        }
+
+        private void cboGioiTinh_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtMaSV.Clear();
+            txtHoTen.Clear();
             cboLop.SelectedIndex = -1;
             cboGioiTinh.SelectedIndex = -1;
             selectedId = -1;
@@ -94,6 +170,28 @@ namespace login
                     dtpNgaySinh.Value = Convert.ToDateTime(row.Cells["ngaysinh"].Value);
                     cboLop.SelectedValue = row.Cells["malop"].Value?.ToString();
                 }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (selectedId == -1)
+            {
+                MessageBox.Show("Vui lòng chọn sinh viên cần sửa từ danh sách!");
+                return;
+            }
+            DatabaseDataContext db = new DatabaseDataContext();
+            var sv = db.tbl_sinhviens.SingleOrDefault(x => x.id == selectedId);
+            if (sv != null)
+            {
+                sv.masv = txtMaSV.Text;
+                sv.hoten = txtHoTen.Text;
+                if (cboGioiTinh.SelectedItem != null) sv.gioitinh = cboGioiTinh.SelectedItem.ToString();
+                sv.ngaysinh = dtpNgaySinh.Value;
+                if (cboLop.SelectedValue != null) sv.malop = cboLop.SelectedValue.ToString();
+                db.SubmitChanges();
+                MessageBox.Show("Sửa thông tin sinh viên thành công!");
+                LoadData();
             }
         }
     }
